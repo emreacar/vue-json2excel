986f8f53596d1a3cd952aeeeb786311f72da456e
mracar <info@emreacar.com.tr>
2020-04-28 01:53:19 +0300

    Working Example

--- vue-json2excel.vue
+++ vue-json2excel.vue
@@ -27,7 +27,41 @@ export default {
   },
   methods: {
     exportExcel () {
-      alert('OK')
+      let xlsHeader = []
+      let xlsRows = []
+      if (!this.columns.length) {
+        this.$emit('error', 'Add Columns!')
+        return
+      }
+
+      if (!this.rows.length) {
+        this.$emit('error', 'Data is empty!')
+        return
+      }
+
+      xlsHeader = this.columns.map((c) => {
+        return c.label || ''
+      })
+
+      xlsRows.push(xlsHeader)
+
+      this.rows.forEach((row, rowIndex) => {
+        let rowData = []
+        this.columns.forEach((column, colIndex) => {
+          if (column.formatter && typeof column.formatter === 'function') {
+            rowData.push(column.formatter(row[column.key], row))
+          } else {
+            rowData.push(row[column.key])
+          }
+        })
+        xlsRows.push(rowData)
+      })
+
+      const exportFilename = this.filename + '.xlsx'
+      const book = XLSX.utils.book_new()
+      const sheet = XLSX.utils.aoa_to_sheet(xlsRows)
+      XLSX.utils.book_append_sheet(book, sheet, this.filename)
+      XLSX.writeFile(book, exportFilename)
     }
   }
 }
